b25ffd4a338eb0715d81f9e1e8e57a82896e780c
Robert Niimi <robertn702@gmail.com>
2016-03-09 07:44:53 -0800

    first commit

---
+++ server.py
@@ -0,0 +1,28 @@
+from config import WATCH_DIR
+from urlparse import urlparse, parse_qs
+import subprocess
+import re
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/download")
+def download():
+    print '/download'
+    url = request.args.get('url')
+    print 'url: ' + url
+    rawurl = re.sub(r'\?.*', '', url)
+    print 'rawurl: ' + rawurl
+    title = parse_qs(urlparse(url).query)['title'][0]
+    print 'title:'
+    print title
+
+
+  # subprocess.call(['wget', str(url), '-P', WATCH_DIR])
+    subprocess.call([
+        'curl', '-L', '-o', WATCH_DIR + title + '.torrent', '--compressed', rawurl
+    ])
+
+if __name__ == "__main__":
+    app.run()
